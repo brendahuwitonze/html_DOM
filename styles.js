fa31dd9cb73d6e9ba4b873ab66a6c7277d9c0924
brendahuwitonze <uwitonzebrendah@gmail.com>
2024-04-17 11:40:32 +0200

    changing colors of the list when clicked using the for loop

--- styles.js
+++ styles.js
@@ -6,19 +6,23 @@
 // const classAvailable = classes.classList
 // classes.classList.add('text-book')
 // console.log(classAvailable)
-// let li = document.createElement('li')
-// li.textContent ='home'
-// menu.appendChild(li)
+let li = document.createElement('li')
+li.textContent ='home'
+menu.appendChild(li)
 
-// li = document.createElement('li')
-// li.textContent="about-us"
+li = document.createElement('li')
+li.textContent="about-us"
 
-// let list = document.querySelector('#menu')
-// menu.appendChild(li)
-
-
+let list = document.querySelector('#menu')
+menu.appendChild(li)
 const paragraph = document.querySelector('#main')
-const myParagraph = document.createElement('p')
-myParagraph.textContent= 'This  is a new element'
-myParagraph.style.color = 'red'
-paragraph.appendChild(myParagraph)
+// const myParagraph = document.createElement('p')
+// myParagraph.textContent= 'This  is a new element'
+// myParagraph.style.color = 'red'
+// paragraph.appendChild(myParagraph)
+
+for(let i=0 ;i<=1;i++){
+    list.addEventListener('click',function(){
+        list.style.color ='red'
+    })
+}
