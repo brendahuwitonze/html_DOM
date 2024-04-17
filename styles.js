2d96ae9ecb726160a9a62e858b4fe4bb00ae595c
brendahuwitonze <uwitonzebrendah@gmail.com>
2024-04-17 10:04:22 +0200

    selected some elements and tried to add classes inside and also to style

---
+++ styles.js
@@ -0,0 +1,9 @@
+
+
+// const pText = document.querySelector('#message');
+// pText.style.cssText= 'color:blue'
+
+const classes = document.querySelector('#para');
+const classAvailable = classes.classList
+classes.classList.add('text-book')
+console.log(classAvailable)
