a46b4ef200cd6a648370e6c91fa4917417ecdbd6
brendahuwitonze <uwitonzebrendah@gmail.com>
2024-04-17 10:32:23 +0200

    added the list to the ul element

--- styles.js
+++ styles.js
@@ -1,9 +1,17 @@
 
+// const pText = document.querySelector('#message');
+// pText.style.cssText= 'color:blue'
 
-const pText = document.querySelector('#message');
-pText.style.cssText= 'color:blue'
+// const classes = document.querySelector('#para');
+// const classAvailable = classes.classList
+// classes.classList.add('text-book')
+// console.log(classAvailable)
+let li = document.createElement('li')
+li.textContent ='home'
+menu.appendChild(li)
 
-const classes = document.querySelector('#para');
-const classAvailable = classes.classList
-classes.classList.add('text-book')
-console.log(classAvailable)
+li = document.createElement('li')
+li.textContent="about-us"
+
+let list = document.querySelector('#menu')
+menu.appendChild(li)
